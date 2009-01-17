bfb9d0970786870da1d33e78807233738981e565
Andrew Rodland <arodland@comcast.net>
2009-01-17 02:03:54 -0600

    Really fix <location></location>

--- WWW-MenuGrinder/lib/WWW/MenuGrinder/Plugin/DefaultTarget.pm
+++ WWW-MenuGrinder/lib/WWW/MenuGrinder/Plugin/DefaultTarget.pm
@@ -12,8 +12,13 @@ sub item_pre_mogrify {
   my ($self, $item) = @_;
 
   if (exists $item->{location} && !exists $item->{target}) {
-    my $loc = ref($item->{location} eq 'ARRAY') ? $item->{location}[0] : $item->{location};
-    $item->{target} = "/" . $loc;
+    if (ref $item->{location} eq "ARRAY") {
+      $item->{target} = "/" . $item->{location}[0];
+    } elsif (ref $item->{location} eq "HASH") {
+      $item->{target} = "/"; # XML::Simple is stupid.
+    } else {
+      $item->{target} = "/" . $item->{location};
+    }
   }
 
   return $item;
