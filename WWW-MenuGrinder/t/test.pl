a2b0ae92fc1cb55f1fee45dd00b3d3613f3cfd0d
Andrew Rodland <andrew@hbslabs.com>
2009-04-17 01:00:18 -0500

    Commit a start towards "testing" that's been floating around a while

--- WWW-MenuGrinder/t/test.pl
+++ WWW-MenuGrinder/t/test.pl
@@ -3,55 +3,36 @@
 use strict;
 use warnings;
 
-
-package Test::MenuGrinder;
-
-use Moose;
-
-extends 'WWW::MenuGrinder';
-
-has 'path' => (
-  is => 'ro',
-  lazy => '1',
-  default => sub { 'user/view' }
-);
-
-sub get_variable {
-  my ($self, $varname) = @_;
-
-  return $self->variables->{$varname};
-}
-
-sub variables {
-  my ($self) = @_;
-
-  return {
-    username => "Suzy Queue"
-  };
-}
-
-package main;
-
 use Data::Dumper;
-
-my $grinder = Test::MenuGrinder->new(
-  config => {
-    plugins => [
-      'XMLLoader',
-      'FileReloader',
-      'DefaultTarget',
-      'Hotkey',
-      'Variables',
-      'ActivePath',
-      'NullOutput'
-    ],
-    filename => 't/menu.xml',
-  },
-);
-
-print Dumper $grinder->get_menu;
-
 use Benchmark;
+
+my $grinder;
+
+use lib 't/lib';
+
+timethis(1, sub {
+    require Test::MenuGrinder;
+    $grinder = Test::MenuGrinder->new(
+      config => {
+        plugins => {
+          loader => 'XMLLoader',
+          on_load => [
+            'DefaultTarget',
+            'Hotkey',
+          ],
+          per_request => [
+            'FileReloader',
+            'Variables',
+            'ActivePath',
+          ],
+        },
+        filename => 't/menu.xml',
+      },
+    );
+
+    print Dumper $grinder->get_menu;
+  }
+);
 
 timethese(-5, {
   get_menu => sub { $a = $grinder->get_menu },
