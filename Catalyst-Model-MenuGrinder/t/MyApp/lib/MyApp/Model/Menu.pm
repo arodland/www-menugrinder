cc3a35294a4e2a4fe0a46f55b43f7fdbae24204d
Andrew Rodland <andrew@hbslabs.com>
2010-03-15 20:12:47 -0500

    Update test app to work with MenuGrinder 0.04

--- Catalyst-Model-MenuGrinder/t/MyApp/lib/MyApp/Model/Menu.pm
+++ Catalyst-Model-MenuGrinder/t/MyApp/lib/MyApp/Model/Menu.pm
@@ -4,14 +4,17 @@ use base qw/Catalyst::Model::MenuGrinder/;
 
 __PACKAGE__->config(
   menu_config => {
-    plugins => [
-      'XMLLoader',
-      'FileReloader',
-      'DefaultTarget',
-      'Variables',
-      'ActivePath',
-      'NullOutput',
-    ],
+    plugins => {
+      loader => 'XMLLoader',
+      on_load => [
+        'DefaultTarget',
+      ],
+      per_request => [
+        'FileReloader',
+        'Variables',
+        'ActivePath',
+      ],
+    },
     filename => MyApp->path_to('root', 'menu.xml'),
   },
 );
